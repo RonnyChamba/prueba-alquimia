a22a4b7ecb33e0dbb878bc5f0608b7b86da585ae
Ronny Chamba <ronnychamba96@gmail.com>
2025-04-23 13:55:53 -0500

    configurar conexion base de datos

---
+++ src/main/java/com/cursos/ec/testalquimia/TestAlquimiaApplication.java
@@ -0,0 +1,13 @@
+package com.cursos.ec.testalquimia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAlquimiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAlquimiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cursos/ec/testalquimia/TestAlquimiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursos.ec.testalquimia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAlquimiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
